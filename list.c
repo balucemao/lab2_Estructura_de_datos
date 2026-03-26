d8c8569ea26e1ecbdfba86e5ac89f997297f9c97
Your Name <you@example.com>
2026-03-26 23:36:18 +0000

    new try

--- list.c
+++ list.c
@@ -44,11 +44,17 @@ List * createList() {
 //   - La segunda función retorna el dato del nodo a continuación del current y actualiza el current para que apunte a ese nodo.
 
 void * firstList(List * list) {
-    return NULL;
+    Node* auxNode = list->head;
+    void* dato = list->head->data;
+    list->next = auxNode;
+    return dato;
 }
 
 void * nextList(List * list) {
-    return NULL;
+    Node* siguiente = list->next;
+    void*¨dato = siguiente->data;
+    list->current = siguiente;
+    return dato;
 }
 
 // 3. Programe las funciones void * lastList(List * list) y void * prevList(List * list).
